545e39b5ab3beb6c9dab397f7c49c2e96b9884c6
Phil Mansfield <mansfield@uchicago.edu>
2014-11-08 23:19:55 -0600

    Expanded functionality of geom.go to support the operations needed by fraction_contained.go

--- geom.go
+++ geom.go
@@ -1,7 +1,11 @@
 package gotetra
 
-func (h *Header) CompressCoords(x, y, z, dx, dy, dz int64) int64 {
-	// Can be spend up with & tricks or conditionals.
+import (
+	"math"
+)
+
+func (h *Header) compressCoords(x, y, z, dx, dy, dz int64) int64 {
+	// Can be send up with & tricks or conditionals.
 	newX := (x + dx + h.CountWidth) % h.CountWidth
 	newY := (y + dy + h.CountWidth) % h.CountWidth
 	newZ := (z + dz + h.CountWidth) % h.CountWidth
@@ -9,16 +13,41 @@ func (h *Header) CompressCoords(x, y, z, dx, dy, dz int64) int64 {
 	return newX + newY * h.CountWidth + newZ * h.CountWidth * h.CountWidth
 }
 
-func (h *Header) TetraCorners(idx int64) (int64, int64, int64) {
+func (h *Header) TetraCorners(idx int64, out []int64) {
 	countArea := h.CountWidth * h.CountWidth
 
 	x := idx % h.CountWidth
 	y := (idx % countArea) / h.CountWidth
 	z := idx / countArea
 
-	c1 := h.CompressCoords(x, y, z, 0, 0, 1)
-	c2 := h.CompressCoords(x, y, z, 0, 1, 0)
-	c3 := h.CompressCoords(x, y, z, 1, 0, 0)
+	out[0] = h.compressCoords(x, y, z, 0, 0, 1)
+	out[1] = h.compressCoords(x, y, z, 0, 1, 0)
+	out[2] = h.compressCoords(x, y, z, 1, 0, 0)
+}
 
-	return c1, c2, c3
+func (h *Header) wrapDist(x1, x2 float64) float64 {
+	var low, high float64
+
+	if x1 < x2 {
+		low, high = x1, x2
+	} else {
+		low, high = x2, x1
+	}
+
+	d1 := high - low
+	d2 := low + h.TotalWidth
+
+	if d1 > d2 {
+		return d2
+	} else {
+		return d1
+	}
+}
+
+func (h *Header) Distance(p1, p2 *Particle) float64 {
+	dx := h.wrapDist(p1.Xs[0], p2.Xs[0])
+	dy := h.wrapDist(p1.Xs[1], p2.Xs[1])
+	dz := h.wrapDist(p1.Xs[2], p2.Xs[2])
+
+	return math.Sqrt(dx * dx + dy * dy + dz * dz)
 }
