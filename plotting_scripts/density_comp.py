116cb1c6af2718013f91e0c3e11f98c2e43d3b55
Phil Mansfield <pmansfie@andrew.cmu.edu>
2014-11-23 16:05:48 -0600

    Cosmetic changes to plotting scripts.

--- plotting_scripts/density_comp.py
+++ plotting_scripts/density_comp.py
@@ -4,32 +4,25 @@ import matplotlib.pyplot as plt
 import sys
 
 
-ngp_rhos, cic_rhos, mc10_rhos, mc100_rhos, mc1000_rhos = parse.get_col(sys.argv[1], 0, 1)
+rhos_mat = np.loadtxt(sys.argv[1]).T
 N = int(sys.argv[2])
 
+ngp_rhos, cic_rhos, mc10_rhos, mc100_rhos = rhos_mat[0], rhos_mat[1], rhos_mat[2], rhos_mat[3]
+
 vals = [N/2]
-rhos = [ngp_rhos, cic_rhos, mc10_rhos, mc100_rhos, mc1000_rhos]
-for rho in rhos:
-    rho = np.reshape(rhos, (N, N, N))
-names = ["NGP", "CIC", "MC10", "MC100", "MC1000"]
+rhos = [ngp_rhos, cic_rhos, mc10_rhos, mc100_rhos]
+
+names = ["NGP", "CIC", "MC10", "MC100"]
 
 cmap_name = "cubehelix"
 
 for val in vals:
     for (rho, name)in zip(rhos, names):
-        plt.figure()
-        plt.title("%s" % name)
-        plt.imshow(np.log10(rhos[:, :, val]), cmap=cmap_name)
-        plt.colorbar()
+        rho = np.clip(np.reshape(rho, (N, N, N)), 10**-2.5, 10**3)
 
         plt.figure()
         plt.title("%s" % name)
-        plt.imshow(np.log10(rhos[:, val, :]), cmap=cmap_name)
-        plt.colorbar()
-
-        plt.figure()
-        plt.title("%s" % name)
-        plt.imshow(np.log10(rhos[:, val, :]), cmap=cmap_name)
+        plt.imshow(np.mean(np.log10(rho[:, :, val-1:val+2]), axis=2), cmap=cmap_name)
         plt.colorbar()
 
 plt.show()
--- plotting_scripts/density_slice.py
+++ plotting_scripts/density_slice.py
@@ -9,7 +9,7 @@ N = int(sys.argv[2])
 rhos = np.reshape(rhos, (N, N, N)) / np.mean(rhos)
 # rhos = np.clip(rhos, np.min(1e-2), np.max(rhos))
 
-rhos = np.reshape(rhos, (N, N, N))
+rhos = np.clip(np.reshape(rhos, (N, N, N)), 10**-2.5, 10**3)
 
 vals = (np.array(range(9)) + 1) * N/10
 cmap_name = "cubehelix" #"gist_heat" #"cubehelix"
@@ -17,7 +17,7 @@ cmap_name = "cubehelix" #"gist_heat" #"cubehelix"
 for val in vals:
     plt.figure()
     plt.title(r"N = %d" % val)
-    plt.imshow(np.log10(rhos[:, val, :]), cmap=cmap_name)
+    plt.imshow(np.log10(rhos[:, :, val]), cmap=cmap_name)
     plt.colorbar()
 
 plt.show()
