6e1c85ba58532c06a4a3e7dbf69621552d3f8822
Phil Mansfield <pmansfie@andrew.cmu.edu>
2015-07-26 20:36:41 -0500

    Fixed compiler errors in los.go that I miraculously missed last time before I committed.

--- los/los.go
+++ los/los.go
@@ -4,6 +4,7 @@ package los
 
 import (
 	"math"
+	"github.com/phil-mansfield/gotetra/los/geom"
 )
 
 // ProfileRing is a ring of uniformly spaced profiles. The reported profiles are
@@ -13,7 +14,7 @@ import (
 // format of the profiles, so they must be accessed with the Retreive method.
 type ProfileRing struct {
 	derivs []float64 // Contiguous block of pofile data. Column-major.
-	Lines []geom
+	Lines []geom.Line
 	bins int // Length of an individual profile.
 	n int // Number of Profiles.
 	lowR, highR, dr float64
@@ -32,13 +33,13 @@ func (p *ProfileRing) Init(lowR, highR float64, bins, n int) {
 	p.Lines = make([]geom.Line, n)
 	for i := 0; i < n; i++ {
 		sin, cos := math.Sincos(p.Angle(i))
-		p.Lines[i].Init(0, 0, )
+		p.Lines[i].Init(0, 0, float32(cos), float32(sin))
 	}
 }
 
 // Insert inserts a plateau with the given radial extent and density to the
 // profile.
-func (p *ProfileRing) Insert(start, end rho float64, i int) {
+func (p *ProfileRing) Insert(start, end, rho float64, i int) {
 	if end < p.lowR || start > p.highR {
 		return
 	}
@@ -46,9 +47,10 @@ func (p *ProfileRing) Insert(start, end rho float64, i int) {
 
 	// You could be a bit more careful with floating point ops here.
 	if start > p.lowR {
-		idx, rem := Math.Modf((start - p.lowR) / dr)
+		fidx, rem := math.Modf((start - p.lowR) / p.dr)
+		idx := int(fidx)
 		p.derivs[i*p.n + idx] += rho * rem
-		if idx < p.depth - 1 {
+		if idx < p.bins - 1 {
 			p.derivs[idx*p.n + idx+1] += rho * (1 - rem)
 		}
 	} else {
@@ -56,9 +58,10 @@ func (p *ProfileRing) Insert(start, end rho float64, i int) {
 	}
 
 	if end < p.highR {
-		idx, rem := Math.Modf((end - p.lowR) / dr)
+		fidx, rem := math.Modf((end - p.lowR) / p.dr)
+		idx := int(fidx)
 		p.derivs[i*p.n + idx] -= rho * (1 - rem)
-		if idx < p.depth - 1 {
+		if idx < p.bins - 1 {
 			p.derivs[idx*p.n + idx+1] -= rho * rem
 		}
 	}
@@ -67,14 +70,14 @@ func (p *ProfileRing) Insert(start, end rho float64, i int) {
 // Retrieve does any neccessary post-processing on the specified profile and
 // writes in to an out buffer.
 func (p *ProfileRing) Retrieve(i int, out []float64) {
-	sum := 0
-	for j := 0; j < p.depth; j++ {
-		sum += p.derivs[i + p.N*j]
+	sum := float64(0)
+	for j := 0; j < p.bins; j++ {
+		sum += p.derivs[i + p.n*j]
 		out[j] = sum
 	}
 }
 
 // Angle returns the angle that the line with the specified index points in.
 func (p *ProfileRing) Angle(i int) float64 {
-	return math.Pi * 2 * float64(n) / float64(p.n)
+	return math.Pi * 2 * float64(i) / float64(p.n)
 }
