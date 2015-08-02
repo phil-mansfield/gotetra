4f51790c6cc71e16758b21b53c0d49bb243309ac
Phil Mansfield <mansfield@uchicago.edu>
2015-08-02 13:16:52 -0500

    Parallelized Clear(). Committing so it can be tested on Midway.

--- los/halo_test.go
+++ los/halo_test.go
@@ -1,6 +1,7 @@
 package los
 
 import (
+	"runtime"
 	"testing"
 
 	"github.com/phil-mansfield/gotetra/los/geom"
@@ -39,6 +40,14 @@ func BenchmarkHaloProfilesClear(b *testing.B) {
 	hp := new(HaloProfiles)
 	hp.Init(0, 10, &geom.Vec{0, 0, 0}, 0, 1, 200, 1000)
 	for i := 0; i < b.N; i++ { hp.Clear() }
+}
+
+func BenchmarkHaloProfilesParallelClear(b *testing.B) {
+	hs := make([]HaloProfiles, runtime.NumCPU())
+	for i := range hs { 
+		hs[i].Init(0, 10, &geom.Vec{0, 0, 0}, 0, 1, 200, 1000)
+	}
+	for i := 0; i < b.N/len(hs); i++ { ParallelClearHaloProfiles(hs) }
 }
 
 func BenchmarkHaloProfilesAdd(b *testing.B) {
